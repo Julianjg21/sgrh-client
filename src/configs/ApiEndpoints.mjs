7df188fdb91df10587a45d6c76084a440cd2bec9
Julianjg21 <71424628+Julianjg21@users.noreply.github.com>
2024-05-30 02:18:55 -0500

    added server url

--- src/configs/ApiEndpoints.mjs
+++ src/configs/ApiEndpoints.mjs
@@ -1,5 +1,5 @@
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3080';
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://sgrh-server-128231344b73.herokuapp.com';
 const API_ROUTES = {
     
     //LOGIN
